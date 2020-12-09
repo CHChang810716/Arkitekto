5bf6cc19752451b30553f6d14044d99f38748dfa
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-09 22:37:49 +0800

    fix build failed

--- Arkitekto/exclude_targets_in.cmake
+++ Arkitekto/exclude_targets_in.cmake
@@ -5,7 +5,7 @@ macro(akt_exclude_targets_in dir)
     file(GLOB_RECURSE AKT_EXCLUDED_TARGETS_tmp "${dir}/*.cmake")
     list(APPEND AKT_EXCLUDED_TARGETS ${AKT_EXCLUDED_TARGETS_tmp})
     unset(AKT_EXCLUDED_TARGETS_tmp)
-endmacro(
+endmacro()
 
 else()
 endif()
