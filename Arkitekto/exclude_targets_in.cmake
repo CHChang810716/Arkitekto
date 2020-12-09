4e3ddb4a3de6ef66e3e4bd4facf2f2a380f77c1d
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-09 22:30:56 +0800

    bugfix: support multiple exclude statement

--- Arkitekto/exclude_targets_in.cmake
+++ Arkitekto/exclude_targets_in.cmake
@@ -2,8 +2,10 @@ if(NOT DEFINED akt_exclude_targets_in_included)
 set(akt_exclude_targets_in_included)
 
 macro(akt_exclude_targets_in dir)
-    file(GLOB_RECURSE AKT_EXCLUDED_TARGETS "${dir}/*.cmake")
-endmacro()
+    file(GLOB_RECURSE AKT_EXCLUDED_TARGETS_tmp "${dir}/*.cmake")
+    list(APPEND AKT_EXCLUDED_TARGETS ${AKT_EXCLUDED_TARGETS_tmp})
+    unset(AKT_EXCLUDED_TARGETS_tmp)
+endmacro(
 
 else()
 endif()
