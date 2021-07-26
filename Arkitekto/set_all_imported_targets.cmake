def1f5f819d0aa0ec6992914ff8c012d2b2d6e1e
Chia-Hua Chang <CHChang810716@gmail.com>
2021-07-27 01:06:18 +0800

    experimental: update set all imported targets

--- Arkitekto/set_all_imported_targets.cmake
+++ Arkitekto/set_all_imported_targets.cmake
@@ -2,7 +2,7 @@ if(NOT DEFINED akt_set_all_imported_targets_included)
 set(akt_set_all_imported_targets_included)
 
 if(NOT DEFINED AKT_ALL_IMPORTED_TARGETS)
-  get_directory_property(AKT_IMPORTED_TARGETS IMPORTED_TARGETS)
+  get_directory_property(AKT_IMPORTED_TARGETS DIRECTORY "${CMAKE_SOURCE_DIR}" IMPORTED_TARGETS)
   akt_show_var_debug(AKT_IMPORTED_TARGETS)
 endif()
 
